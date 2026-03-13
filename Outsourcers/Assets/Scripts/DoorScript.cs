a8dd7d16f4432915ad16d9d72f440f8a53cbbde6
WGrainger64 <nibblesandastro@gmail.com>
2026-03-13 17:47:58 -0600

    Added Ship
    Added a box for the starting area. It opens up when you left click. When Jose gets a model for it I can hide this thing.
    
    Added in Jose's grass and dirt textures

---
+++ Outsourcers/Assets/Scripts/DoorScript.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    public bool doorBool = true;
+    public float doorSpeed = 1.0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float doorCheck = Input.GetAxis("Fire1");
+        float translation = 1.0f * Time.deltaTime;
+
+        if (doorCheck != 0)
+        {
+            doorBool = true;
+        }
+
+        if (doorBool)
+        {
+            if (transform.position.y < 11.2)
+            {
+                transform.Translate(0, translation, 0);
+            }
+        }
+
+    }
+}
